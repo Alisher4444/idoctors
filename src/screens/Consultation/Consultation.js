c8e4c9bee586142f7136a738160f292b125a6146
Алишер <alisher@MacBook-Pro-Aliser.local>
2018-05-13 03:59:34 +0600

    add():fab

--- src/screens/Consultation/Consultation.js
+++ src/screens/Consultation/Consultation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Image, Text, TouchableOpacity } from 'react-native';
+import { Container, Header, View, Button, Icon, Fab } from 'native-base';
 import ScrollTabView from 'react-native-scrollable-tab-view';
 import Completed from '../Completed/Completed';
 import Upcoming from '../Upcoming/Upcoming';
@@ -16,14 +17,44 @@ export default class Consultation extends Component {
       />
     )
   };
+  constructor() {
+    super();
+    this.state = {
+      active: 'true'
+    };
+  }
   render() {
     const { navigation } = this.props;
     return (
-      <ScrollTabView>
-        <Completed tabLabel="Completed" navigation={navigation} />
-        <Upcoming tabLabel="Upcoming" navigation={navigation} />
-        <Canceled tabLabel="Canceled" navigation={navigation} />
-      </ScrollTabView>
+      <Container>
+        <ScrollTabView>
+          <Completed tabLabel="Completed" navigation={navigation} />
+          <Upcoming tabLabel="Upcoming" navigation={navigation} />
+          <Canceled tabLabel="Canceled" navigation={navigation} />
+        </ScrollTabView>
+
+        <View style={{ flex: 1 }}>
+          <Fab
+            active={this.state.active}
+            direction="up"
+            containerStyle={{}}
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => this.setState({ active: !this.state.active })}
+          >
+            <Icon name="share" />
+            <Button style={{ backgroundColor: '#34A34F' }}>
+              <Icon name="logo-whatsapp" />
+            </Button>
+            <Button style={{ backgroundColor: '#3B5998' }}>
+              <Icon name="logo-facebook" />
+            </Button>
+            <Button disabled style={{ backgroundColor: '#DD5144' }}>
+              <Icon name="mail" />
+            </Button>
+          </Fab>
+        </View>
+      </Container>
     );
   }
 }
